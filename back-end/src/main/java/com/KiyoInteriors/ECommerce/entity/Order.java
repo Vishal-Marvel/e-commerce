0f7adc1095b9b62277b110537b3723cca934509c
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 15:02:43 +0530

    Update Order.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/Order.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/Order.java
@@ -7,7 +7,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-
+/**
+ * This class represents an image file that can be stored and retrieved from a database.
+ * It has three fields: fileName, data and contentType.
+ * The fileName is the name of the image file, such as "picture.jpg".
+ * The data is an array of bytes that contains the binary content of the image file.
+ * The contentType is a string that indicates the MIME type of the image file, such as "image/jpeg".
+ */
 @Data
 @Document
 public class Order {

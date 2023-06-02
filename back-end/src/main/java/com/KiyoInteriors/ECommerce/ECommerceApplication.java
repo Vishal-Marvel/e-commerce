eb4d9fc01ada22c888b26cc8c92a9c6b54c144eb
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 21:33:45 +0530

    Update ECommerceApplication.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/ECommerceApplication.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/ECommerceApplication.java
@@ -2,7 +2,11 @@ package com.KiyoInteriors.ECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
 
+The "UserService" class provides methods for updating user details and converting a User object to a UserResponse object.
+updateUser(UserRequest userDTO, String id): Updates the user details based on the provided UserRequest object and user ID.
+*/
 @SpringBootApplication
 public class ECommerceApplication {
 

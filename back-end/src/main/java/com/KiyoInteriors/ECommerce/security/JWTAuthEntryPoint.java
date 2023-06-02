febebcb73a58dfa7f13b12f6eb73ae040b81d490
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 21:23:42 +0530

    Update JWTAuthEntryPoint.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/security/JWTAuthEntryPoint.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/security/JWTAuthEntryPoint.java
@@ -8,7 +8,15 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+/**
 
+The "JWTAuthEntryPoint" class is an implementation of the "AuthenticationEntryPoint" interface.
+It handles unauthorized access to protected resources in a web application using JSON Web Tokens (JWT) for authentication.
+The "commence" method is overridden to send an HTTP response with the appropriate error code and message when authentication fails.
+It sends an HTTP 401 Unauthorized status code and includes the error message from the "AuthenticationException" in the response.
+This class is typically used in conjunction with a JWT-based authentication mechanism to handle unauthorized requests.
+When an unauthenticated request is made to a protected resource, this entry point is invoked to send an error response.
+*/
 @Component
 public class JWTAuthEntryPoint implements AuthenticationEntryPoint {
     @Override

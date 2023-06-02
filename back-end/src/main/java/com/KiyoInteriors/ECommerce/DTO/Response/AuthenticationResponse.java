a33b99962f3142deb142e1ded0fb3713da7be580
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 14:16:23 +0530

    Update AuthenticationResponse.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/AuthenticationResponse.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/AuthenticationResponse.java
@@ -2,7 +2,10 @@ package com.KiyoInteriors.ECommerce.DTO.Response;
 
 import lombok.Builder;
 import lombok.Data;
-
+/**
+ * This class represents an authentication response that contains a token and a role for a user.
+ * It uses the @Data and @Builder annotations from Lombok to generate getters, setters, constructors, and builders for the fields.
+ */
 @Data
 @Builder
 public class AuthenticationResponse {

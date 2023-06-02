e10bef140896c1fbb3b56fc9794a72c1751424f7
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 14:13:31 +0530

    Update ProductRequest.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ProductRequest.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ProductRequest.java
@@ -5,7 +5,19 @@ import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+/**
+ * This class represents a product request that contains the information
+ * about a product to be added to the online store.
+ * It has the following fields:
+ * - productName: the name of the product
+ * - productDescription: a brief description of the product
+ * - productPrize: the price of the product in dollars
+ * - category: the category of the product (e.g. clothing, electronics, etc.)
+ * - productPics: a list of files that contain the images of the product
+ * - model: an optional field that specifies the model of the product (e.g. iPhone 14, Nike Air Max, etc.)
+ * - sizes: an optional list of strings that indicate the available sizes of the product (e.g. S, M, L, XL, etc.)
+ * - colors: an optional list of strings that indicate the available colors of the product (e.g. black, white, red, etc.)
+ */
 @Data
 public class ProductRequest {
     @NotBlank(message = "Name is necessary")

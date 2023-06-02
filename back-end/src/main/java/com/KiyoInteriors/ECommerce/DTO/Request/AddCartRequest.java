d2c13f0c917278da9cac3a7068a075037b6f1cb5
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 14:09:01 +0530

    Update AddCartRequest.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/AddCartRequest.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/AddCartRequest.java
@@ -1,11 +1,18 @@
 package com.KiyoInteriors.ECommerce.DTO.Request;
 
 import lombok.Data;
-
-@Data
+/**
+ * This class represents a request to add a product to the shopping cart.
+ * It contains the following fields:
+ * - productId: the unique identifier of the product
+ * - size: the size of the product (e.g. S, M, L, XL)
+ * - quantity: the number of units to add
+ * - color: the color of the product (e.g. red, blue, green)
+ */
+@Data 
 public class AddCartRequest {
-    private String productId;
-    private String size;
-    private Integer quantity;
-    private String color;
+    private  String productId;
+    private  String size;
+    private  Integer quantity;
+    private  String color;
 }

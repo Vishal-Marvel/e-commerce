8929afcf306ffdedf57544720b9d8a3aefa755b8
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-02 14:25:56 +0530

    Update ProductResponse.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/ProductResponse.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/ProductResponse.java
@@ -6,18 +6,21 @@ import com.KiyoInteriors.ECommerce.entity.ProductSize;
 import lombok.Data;
 
 import java.util.List;
-
+/**
+ * A class that represents a product response object.
+ * It contains the fields and constructor of a product entity.
+ */
 @Data
 public class ProductResponse {
-    private String id;
-    private String productName;
-    private String productDescription;
-    private Double productPrize;
-    private String category;
-    private List<Image> productPics;
-    private String model;
-    private List<String> sizes;
-    private List<String> colors;
+    private String id; // the unique identifier of the product
+    private String productName; // the name of the product
+    private String productDescription; // the description of the product
+    private Double productPrize; // the prize of the product in dollars
+    private String category; // the category of the product (e.g. clothing, electronics, etc.)
+    private List<Image> productPics; // the list of images of the product
+    private String model; // the model of the product (e.g. iPhone 14, Nike Air Max, etc.)
+    private List<String> sizes; // the list of available sizes of the product (e.g. S, M, L, etc.)
+    private List<String> colors; // the list of available colors of the product (e.g. black, white, red, etc.)
 
     public ProductResponse(Product product) {
         this.id = product.getId();

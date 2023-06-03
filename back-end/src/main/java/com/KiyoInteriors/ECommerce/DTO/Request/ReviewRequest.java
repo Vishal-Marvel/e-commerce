8af720d3391ac820b976af19131ba80732e2cb92
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-03 10:53:40 +0530

    Update ReviewRequest.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ReviewRequest.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ReviewRequest.java
@@ -2,7 +2,17 @@ package com.KiyoInteriors.ECommerce.DTO.Request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+/**
 
+This class represents a review request for a product.
+It contains the following fields:
+productId: a string representing the ID of the product being reviewed.
+review: a string representing the review text provided by the user.
+rating: an integer representing the rating given to the product, ranging from 1 to 5.
+The ReviewRequest class is used to gather information about a product review
+in order to add it to the system. It ensures that the rating field is not blank
+and requires a valid rating value out of 5.
+*/
 @Data
 public class ReviewRequest
 {

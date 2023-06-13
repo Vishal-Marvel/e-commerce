2ea4f671436202a1cb6e0455db584398788c7721
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:10:21 +0530

    Update ItemNotFoundException.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/exceptions/ItemNotFoundException.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/exceptions/ItemNotFoundException.java
@@ -2,7 +2,11 @@ package com.KiyoInteriors.ECommerce.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/**
+ * An exception class representing an item not found exception.
+ * This class extends the RuntimeException class and is used to handle exceptions when an item is not found.
+ * It holds information about the exception message.
+ */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ItemNotFoundException extends RuntimeException{
     public ItemNotFoundException(String message) {

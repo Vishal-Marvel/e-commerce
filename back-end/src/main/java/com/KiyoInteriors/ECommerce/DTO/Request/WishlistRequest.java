d7d6d2844e5f5035c20f20932db0307d4fda78ab
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 22:55:49 +0530

    Update WishlistRequest.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/WishlistRequest.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/WishlistRequest.java
@@ -1,7 +1,13 @@
 package com.KiyoInteriors.ECommerce.DTO.Request;
 
 import lombok.Data;
-
+/**
+ * A class representing a Wishlist request.
+ * This class encapsulates the information required to add a product to the wishlist.
+*/
+/**
+ * The ID of the product to be added to the wishlist.
+ */
 @Data
 public class WishlistRequest {
     private String productId;

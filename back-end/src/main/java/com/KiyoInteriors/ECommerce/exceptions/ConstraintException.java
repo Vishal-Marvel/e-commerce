4cb0b01ead7d9ae8609f3523db3b3d350ecb9753
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:09:48 +0530

    Update ConstraintException.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/exceptions/ConstraintException.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/exceptions/ConstraintException.java
@@ -3,7 +3,11 @@ package com.KiyoInteriors.ECommerce.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/**
+ * An exception class representing a constraint exception.
+ * This class extends the RuntimeException class and is used to handle constraint-related exceptions.
+ * It holds information about the exception message.
+ */
 @ResponseStatus(value = HttpStatus.CONFLICT)
 public class ConstraintException extends RuntimeException{
     public ConstraintException(String message) {

1cc7a4e75f1b6ca27bc41c02019685c412306bd5
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:08:08 +0530

    Update RawImage.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/RawImage.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/RawImage.java
@@ -5,7 +5,10 @@ import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
-
+/**
+ * A class representing a raw image.
+ * This class holds the ID and data of a raw image, where the data is stored as a byte array.
+ */
 @Data
 @Document
 public class RawImage {

d93dc7cedaa0ae96a61ae4a94df7e959834d3513
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 22:59:09 +0530

    Update ResetPasswordRequest.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ResetPasswordRequest.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Request/ResetPasswordRequest.java
@@ -1,7 +1,10 @@
 package com.KiyoInteriors.ECommerce.DTO.Request;
 
 import lombok.Data;
-
+/**
+ * A class representing a Reset Password request.
+ * This class encapsulates the information required to reset a user's password.
+ */
 @Data
 public class ResetPasswordRequest {
     private String usernameOrEmail;

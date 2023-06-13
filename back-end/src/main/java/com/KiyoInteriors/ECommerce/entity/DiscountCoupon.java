f0396e3ed6bdf1af5099085505301779776bdde3
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:07:33 +0530

    Update DiscountCoupon.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/DiscountCoupon.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/entity/DiscountCoupon.java
@@ -6,7 +6,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
-
+/**
+ * A class representing a discount coupon.
+ * This class holds information about a discount coupon, including its code, discount percentage,
+ * applicable products, and validity period.
+ */
 @Document
 @Data
 public class DiscountCoupon {

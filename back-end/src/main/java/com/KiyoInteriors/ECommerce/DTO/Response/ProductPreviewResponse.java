df273b76b4be7e42c6b83d2b7e8855e29bd9491f
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:04:24 +0530

    Update ProductPreviewResponse.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/ProductPreviewResponse.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/DTO/Response/ProductPreviewResponse.java
@@ -2,7 +2,10 @@ package com.KiyoInteriors.ECommerce.DTO.Response;
 
 import lombok.Builder;
 import lombok.Data;
-
+/**
+ * A class representing a Product Preview Response.
+ * This class encapsulates the information related to a product preview, including its ID, name, and image.
+ */
 @Data
 @Builder
 public class ProductPreviewResponse {

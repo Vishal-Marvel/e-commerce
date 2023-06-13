80d944b8464f9a1ace1bb9f7a734907c22f086d0
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-13 23:11:27 +0530

    Update DiscountCouponRepository.java

--- back-end/src/main/java/com/KiyoInteriors/ECommerce/repository/DiscountCouponRepository.java
+++ back-end/src/main/java/com/KiyoInteriors/ECommerce/repository/DiscountCouponRepository.java
@@ -4,7 +4,10 @@ import com.KiyoInteriors.ECommerce.entity.DiscountCoupon;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
-
+/**
+ * A repository interface for managing discount coupons in a MongoDB database.
+ * This interface extends the MongoRepository interface, providing CRUD operations and query methods for the DiscountCoupon entity.
+ */
 public interface DiscountCouponRepository extends MongoRepository<DiscountCoupon, String> {
     boolean existsByCouponCode(String coupon);
     Optional<DiscountCoupon> findByCouponCode(String coupon);

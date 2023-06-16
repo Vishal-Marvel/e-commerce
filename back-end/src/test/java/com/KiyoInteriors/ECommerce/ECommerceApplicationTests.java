82d4b0ed62fdf0c7f52eb83a36454d52348b2729
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-06-16 13:54:16 +0530

    Update ECommerceApplicationTests.java

--- back-end/src/test/java/com/KiyoInteriors/ECommerce/ECommerceApplicationTests.java
+++ back-end/src/test/java/com/KiyoInteriors/ECommerce/ECommerceApplicationTests.java
@@ -2,6 +2,11 @@ package com.KiyoInteriors.ECommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * This class represents a test for the context loading of the ECommerceApplication.
+ * It contains the following method:
+ * - contextLoads(): tests if the application context loads correctly
+ */
 
 @SpringBootTest
 class ECommerceApplicationTests {
